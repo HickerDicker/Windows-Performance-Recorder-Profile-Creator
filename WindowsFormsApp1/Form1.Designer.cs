320872976e99e4f6009cc0bab4e34261a9562431
Hickensa <113861320+HickerDicker@users.noreply.github.com>
2025-04-27 18:41:13 +0300

    first commit commit github

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Checkbox = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.GUID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.searchBox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1053, 56);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(465, 30);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Windows Performance Recorder Profile Creator";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button2.BackgroundImage")));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Josefin Sans SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(955, 1);
+            this.button2.Margin = new System.Windows.Forms.Padding(1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(49, 49);
+            this.button2.TabIndex = 11;
+            this.button2.TabStop = false;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Josefin Sans SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1006, 1);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(49, 49);
+            this.button1.TabIndex = 10;
+            this.button1.TabStop = false;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Checkbox,
+            this.Name,
+            this.GUID});
+            this.listView1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.ForeColor = System.Drawing.Color.White;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 81);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(1053, 573);
+            this.listView1.TabIndex = 12;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Checkbox
+            // 
+            this.Checkbox.Text = "Checkbox";
+            // 
+            // Name
+            //
+            this.Name.Text = "Name";
+            // 
+            // GUID
+            // 
+            this.GUID.Text = "GUID";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(42)))), ((int)(((byte)(42)))));
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(891, 662);
+            this.button5.Margin = new System.Windows.Forms.Padding(1);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(146, 32);
+            this.button5.TabIndex = 57;
+            this.button5.TabStop = false;
+            this.button5.Text = "Create WPR Profile";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.searchBox);
+            this.panel2.Location = new System.Drawing.Point(-27, 57);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1061, 50);
+            this.panel2.TabIndex = 58;
+            // 
+            // searchBox
+            // 
+            this.searchBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.searchBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchBox.ForeColor = System.Drawing.Color.White;
+            this.searchBox.Location = new System.Drawing.Point(884, 5);
+            this.searchBox.Margin = new System.Windows.Forms.Padding(0);
+            this.searchBox.Name = "searchBox";
+            this.searchBox.Size = new System.Drawing.Size(169, 27);
+            this.searchBox.TabIndex = 0;
+            this.searchBox.Text = "Search";
+            this.searchBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(1053, 704);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            // Usually right here a line would spawn this.Name = "Form1" and it would cause a conflict with the listView removing it fixes the issue so if you are trying to compile this yourself there you go
+            // while yes I could have just changed the name of the column Too Bad! if it works it works dont touch it they say so uuh diy
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Checkbox;
+        private System.Windows.Forms.ColumnHeader Name;
+        private System.Windows.Forms.ColumnHeader GUID;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,277 @@
+﻿// in case Rain sees this I tried my best to keep everything camelCase we love Joe Camel
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private List<ListViewItem> allListViewItems;
+        // stolen from stack overflow
+
+        private Point mouseOffset;
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+(
+    int nLeftRect,     // x-coordinate of upper-left corner
+    int nTopRect,      // y-coordinate of upper-left corner
+    int nRightRect,    // x-coordinate of lower-right corner
+    int nBottomRect,   // y-coordinate of lower-right corner
+    int nWidthEllipse, // width of ellipse
+    int nHeightEllipse // height of ellipse
+);
+        public void loadProvidersIntoListView()
+        {
+            listView1.View = View.Details;
+            listView1.CheckBoxes = true;
+            listView1.Columns.Clear();
+            listView1.Columns.Add("Checkbox", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Name", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("GUID", -2, HorizontalAlignment.Left);
+            listView1.Items.Clear();
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = @"C:\Windows\System32\logman.exe",
+                Arguments = "query providers",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            try
+            {
+                using (Process process = Process.Start(psi))
+                {
+                    string output = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+                    List<providerInfo> providers = parseLogmanOutput(output);
+                    foreach (providerInfo provider in providers)
+                    {
+                        ListViewItem item = new ListViewItem
+                        {
+                            Checked = false,
+                            Text = ""
+                        };
+                        item.SubItems.Add(provider.Name);
+                        item.SubItems.Add(provider.Guid);
+                        listView1.Items.Add(item);
+                    }
+                    // The command completed succcessfully appears as an entry if I doont do this LMAO
+                    if (listView1.Items.Count > 0)
+                    {
+                        listView1.Items.RemoveAt(listView1.Items.Count - 1);
+                    }
+
+                    listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                    listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+                    allListViewItems = listView1.Items.Cast<ListViewItem>().ToList();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error querying providers: {ex.Message}");
+            }
+        }
+
+        private List<providerInfo> parseLogmanOutput(string output)
+        {
+            List<providerInfo> providers = new List<providerInfo>();
+            string[] lines = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string line in lines)
+            {
+                if (line.Contains("Provider") && line.Contains("GUID"))
+                    continue;
+
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length >= 2)
+                {
+                    string guid = parts[parts.Length - 1].Trim();
+                    string name = string.Join(" ", parts, 0, parts.Length - 1).Trim().Trim('-'); // the first entry without this would have -------------------------- and I no no like it
+
+                    providers.Add(new providerInfo { Name = name, Guid = guid });
+                }
+            }
+
+            return providers;
+        }
+
+
+        private class providerInfo
+        {
+            public string Name { get; set; }
+            public string Guid { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            loadProvidersIntoListView();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+        }
+        private const int CS_DROPSHADOW = 0x20000;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ClassStyle |= CS_DROPSHADOW;
+                return cp;
+            }
+        }
+        // ts is needed to be able to move the window around GunaUI users dont have to worry about these also stolen from stack overflow
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void panel1_Paint(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            var checkedItems = listView1.CheckedItems;
+            if (checkedItems.Count == 0)
+            {
+                MessageBox.Show("Please select a provider.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            foreach (ListViewItem item in checkedItems)
+            {
+                var originalName = item.SubItems[1].Text;
+                var guid = item.SubItems[2].Text;
+                var invalidChars = Path.GetInvalidFileNameChars();
+                var pattern = $"[{Regex.Escape(new string(invalidChars))}]";
+                var provName = Regex.Replace(originalName, pattern, "_");
+                provName = Regex.Replace(provName, @"[\s:]+", "_"); // we need to follow the format of wpr RAH!
+                try
+                {
+                    var xml = generateWprpProfile(provName, guid);
+                    var outputFilePath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), $"{provName}.wprp");
+                    File.WriteAllText(outputFilePath, xml, Encoding.UTF8);
+                    item.Checked = false;
+                    MessageBox.Show($"Profile generated successfully: {outputFilePath}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Failed to generate profile for {originalName}: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+
+
+        private string generateWprpProfile(string providerName, string providerGuid)
+        {
+            var user = Environment.UserName;
+            return $@"<?xml version=""1.0"" encoding=""UTF-8"" standalone=""yes""?>
+<WindowsPerformanceRecorder Version=""1.0"" Author=""{user}"" Company="""">
+  <Profiles>
+    <!-- System session -->
+    <SystemCollector Id=""SystemCollector"" Name=""System Collector"">
+      <BufferSize Value=""256""/>
+      <Buffers Value=""3"" PercentageOfTotalMemory=""true"" MaximumBufferSpace=""256""/>
+    </SystemCollector>
+
+    <!-- ETW session for nonpaged-pool providers -->
+    <EventCollector Id=""EventCollector-NonpagedPool"" Name=""Event Collector NonpagedPool"">
+      <BufferSize Value=""256""/>
+      <Buffers Value=""3"" PercentageOfTotalMemory=""true"" MaximumBufferSpace=""64""/>
+    </EventCollector>
+
+    <!-- Kernel events (optional) -->
+    <SystemProvider Id=""SystemProvider"">
+      <Keywords>
+        <Keyword Value=""Loader""/>
+        <Keyword Value=""ProcessThread""/>
+      </Keywords>
+    </SystemProvider>
+
+    <!-- Custom provider -->
+    <EventProvider
+      Id=""Manifested/{providerName}""
+      Name=""{providerGuid}""
+      Level=""5""
+      NonPagedMemory=""true""
+      Stack=""true""/>
+
+    <!-- Verbose/Memory profile -->
+    <Profile
+      Id=""{providerName}.Verbose.Memory""
+      Name=""{providerName}""
+      LoggingMode=""Memory""
+      DetailLevel=""Verbose""
+      Description=""Capture all {providerName} events in memory"">
+      <Collectors>
+        <SystemCollectorId Value=""SystemCollector"">
+          <SystemProviderId Value=""SystemProvider""/>
+        </SystemCollectorId>
+        <EventCollectorId Value=""EventCollector-NonpagedPool"">
+          <EventProviders>
+            <EventProviderId Value=""Manifested/{providerName}""/>
+          </EventProviders>
+        </EventCollectorId>
+      </Collectors>
+    </Profile>
+  </Profiles>
+</WindowsPerformanceRecorder>";
+        }
+        // this is literally just a search bar I just wanted to add a code comment here thats it thank you! 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = searchBox.Text.ToLower();
+
+            listView1.Items.Clear();
+
+            foreach (var item in allListViewItems)
+            {
+                string itemName = item.SubItems[1].Text.ToLower();
+
+                if (itemName.Contains(searchText))
+                {
+                    listView1.Items.Add(item);
+                }
+            }
+        }
+    }
+}
